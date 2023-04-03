e8eb665b2aea9d335346e040de98c8a9551c5616
Sibber Ahmad <117057217+fieryant@users.noreply.github.com>
2023-04-03 16:12:54 +0000

    print with priority queue

--- prims_spanning_tree.cpp
+++ prims_spanning_tree.cpp
@@ -1,43 +1,72 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+// #include <bitset>
 using namespace std;
+
 #define V 5
-#define INF numeric_limits<int>::max()
-int main(){
-	/*
-	int graph[V][V] =
+#define INF INT_MAX
+
+typedef pair<int, int> pi;
+
+typedef priority_queue<pi, vector<pi>, greater<pi>> min_pq;
+
+int graph[V][V] = {
+	{0, 9, 75, 0, 0},
+	{9, 0, 95, 19, 42},
+	{75, 95, 0, 51, 66},
+	{0, 19, 51, 0, 31},
+	{0, 42, 66, 31, 0}};
+min_pq pq;
+
+void printPQ()
+{
+	cout << endl;
+	while (!pq.empty())
 	{
-	{0,0,5,8,4},//
-	{0,0,2,0,1},
-	{5,2,0,3,4},
-	{8,0,3,0,4},
-	{4,1,4,4,0}
-	};
-	*/
-	int graph[V][V] = {
-	  {0, 9, 75, 0, 0},
-	  {9, 0, 95, 19, 42},
-	  {75, 95, 0, 51, 66},
-	  {0, 19, 51, 0, 31},
-	  {0, 42, 66, 31, 0}
-  };
-	int weight[V];
-	int node = 0;
-	memset(weight, INF,V);
-	
-	while(node < V - 1){
-		int short_node = INF;
-		int current_node = node;
-		for(int i = node; i < V; i++){
-			int current_weight = graph[i][node];
-			if(current_weight){
-				weight[i] = min(weight[i], current_weight);
-				if(current_weight < short_node){
-					short_node = current_weight;
-					current_node = i;
-				}
+		pi top = pq.top();
+		cout << top.first << " " << top.second << endl;
+		pq.pop();
+	}
+	cout << endl;
+}
+
+int VISITED = (1<<V)-1;
+
+int main()
+{
+	int current_node = 0;
+	int visited = 1;
+	int cost = 0;
+	// cout << bitset<V> (VISITED) << endl;
+	while (visited != VISITED)
+	{
+		for (int i = 0; i < V; i++)
+		{
+			// cout << bitset<V> (visited) << " ";
+			// cout << bitset<V> (1<<i) << endl;
+			if ((visited&(1<<i)))
+			{
+				continue;
+			}
+			if (graph[current_node][i])
+			{
+				pq.push(make_pair(graph[current_node][i], i));
 			}
 		}
-		cout << node <<" --> "<< current_node << " " <<": " << short_node << endl;
-		node++;
+		pi top = pq.top();
+		while((visited&(1<<top.second)))
+		{
+			// current_node = top.second;
+			pq.pop();
+			top = pq.top();
+		}
+		
+		visited = (visited|(1<<top.second));
+		cost += top.first;
+		cout << top.second+1 << " : " << top.first << endl;
+		pq.pop();
+		current_node = top.second;
 	}
+	cout << endl << cost << endl;
+	printPQ();
 }
